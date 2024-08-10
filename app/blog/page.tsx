f0a82184e1c7eed38a8940bc40b0fe87d7afa2bc
hisham dulfacker <dulfackerhisham@gmail.com>
2024-08-10 10:41:04 +0530

    useRouter

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,22 @@
-const page = () => {
+"use client"
+import { useRouter } from "next/navigation";
+
+const Page = () => {
+  
+  const router = useRouter();
+
+  const back = () => {
+    router.push("/")
+  }
+
   return (
     <div className="text-center blog-page">
         <h1>Welcome to my Blog</h1>
+
+        <button onClick={back} className="bg-blue-500 p-4 text-white roumded mt-11">Back</button>
+
     </div>
   )
 }
 
-export default page;
+export default Page;
--- app/bmw/page.tsx
+++ app/bmw/page.tsx
@@ -1,7 +1,17 @@
+"use client"
+import { useRouter } from "next/navigation";
+
 const Bmw = () => {
+
+  const router = useRouter();
+
+  const back = () => {
+    router.push("/blog")
+  }
   return (
     <div className="bg-blue-400 text-center p-6">
         <h1>page BMW</h1>
+        <button onClick={back} className="bg-red-500 p-4 text-white roumded mt-11">Back</button>
     </div>
   )
 }
