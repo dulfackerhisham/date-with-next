24bdbaa2478d519f48dbcd63a313348767231582
hisham dulfacker <dulfackerhisham@gmail.com>
2024-08-10 12:02:52 +0530

    post -body

--- app/api/items/route.tsx
+++ app/api/items/route.tsx
@@ -1,10 +1,17 @@
-export async function GET() {
+export async function GET(req) {
+
+    const type = req.nextUrl.searchParams.get('type')
+    console.log("type:", type);
+    
     return Response.json([
         {title: "Speedway"},
         {director: "shamil"}
     ])
 };
 
-export async function POST() {
+export async function POST(req) {
+    const body = await req.json();
+    console.log("body:", body);
+    
     return Response.json({message: "POST success"})
 }
